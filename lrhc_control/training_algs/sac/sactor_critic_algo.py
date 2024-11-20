8a70406dedc72ad2f416e18a907cc2ddbfa1e850
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-11-20 11:10:49 +0100

    default to no exploration noise

--- lrhc_control/training_algs/sac/sactor_critic_algo.py
+++ lrhc_control/training_algs/sac/sactor_critic_algo.py
@@ -1094,7 +1094,7 @@ class SActorCriticAlgoBase(ABC):
         self._log_alpha = None
         self._alpha = 0.2
 
-        self._n_noisy_envs = 50 # n of random envs on which noisy actions will be applied
+        self._n_noisy_envs = 0 # n of random envs on which noisy actions will be applied
         self._noise_freq = 50
         self._noise_duration = 1 # should be less than _noise_freq
 
