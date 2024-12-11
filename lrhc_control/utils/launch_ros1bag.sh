aa573ac2d90e964faeedeea1aa2e96512c634ca4
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-12-11 12:57:23 +0100

    Signed-off-by: Andrea Patrizi <andreapatrizi1b6e6@gmail.com>

--- lrhc_control/utils/launch_ros1bag.sh
+++ lrhc_control/utils/launch_ros1bag.sh
@@ -1,3 +1,54 @@
 #!/bin/bash
 
-echo "Not implemented yet."
+# Function to display usage information
+usage() {
+    echo "Usage: $0 --ns <namespace> --id <bag_id> [--output_path <path>]"
+    exit 1
+}
+
+# Function to handle SIGINT
+sigint_handler() {
+    echo "[launch_rosbag.sh]:SIGINT received, exiting..."
+    exit 0
+}
+# Set the trap to catch SIGINT and call sigint_handler
+trap sigint_handler SIGINT
+
+# Check if the required argument is provided
+if [ "$#" -lt 4 ] || [ "$1" != "--ns" ] || [ "$3" != "--id" ]; then
+    usage
+fi
+
+# Extract the namespace from the arguments
+NAMESPACE=$2
+BAG_ID=$4
+
+# Default output path to /tmp with namespace and date-time
+OUTPUT_PATH="/tmp/rosbag_${NAMESPACE}_$(date +%Y-%m-%d_%H-%M-%S)_${BAG_ID}"
+
+# Check for optional --output_path argument
+if [ "$#" -eq 6 ] && [ "$5" == "--output_path" ]; then
+    OUTPUT_PATH="$6/rosbag_${NAMESPACE}_$(date +%Y-%m-%d_%H-%M-%S)_${BAG_ID}"
+elif [ "$#" -gt 4 ]; then
+    usage
+fi
+
+# Source ROS setup
+source /opt/ros/noetic/setup.bash
+
+# Change to the training data directory
+cd $HOME/training_data
+
+# Define the topics with the namespace replaced
+TOPICS=(
+    "/RHCViz_${NAMESPACE}_HandShake"
+    "/RHCViz_${NAMESPACE}_hl_refs"
+    "/RHCViz_${NAMESPACE}_rhc_actuated_jointnames"
+    "/RHCViz_${NAMESPACE}_rhc_q"
+    "/RHCViz_${NAMESPACE}_rhc_refs"
+    "/RHCViz_${NAMESPACE}_robot_actuated_jointnames"
+    "/RHCViz_${NAMESPACE}_robot_q"
+)
+
+# Record the topics using rosbag
+rosbag record -O "$OUTPUT_PATH" "${TOPICS[@]}"
