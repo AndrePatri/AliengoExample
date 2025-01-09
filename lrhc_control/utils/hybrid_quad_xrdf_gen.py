904b996e62cfa1d5a58786c89db5eeb77a5aef4a
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2025-01-09 15:30:57 +0100

    added battery xacro option + end effector for centauro + removed unnecessary vars

--- lrhc_control/utils/hybrid_quad_xrdf_gen.py
+++ lrhc_control/utils/hybrid_quad_xrdf_gen.py
@@ -41,47 +41,41 @@ def get_xrdf_cmds_horizon(urdf_descr_root_path : str = None):
 
 def get_xrdf_cmds_centauro(urdf_descr_root_path: str = None):
 
-        cmds_aux = []
+        cmds = []
         
-        xrdf_cmd_vals = [True, True, True, False, False, False]
-        legs = "true" if xrdf_cmd_vals[0] else "false"
-        big_wheel = "true" if xrdf_cmd_vals[1] else "false"
-        upper_body ="true" if xrdf_cmd_vals[2] else "false"
-        velodyne = "true" if xrdf_cmd_vals[3] else "false"
-        realsense = "true" if xrdf_cmd_vals[4] else "false"
-        floating_joint = "true" if xrdf_cmd_vals[5] else "false"
+        cmds.append("legs:=true")
+        cmds.append("big_wheel:=true")
+        cmds.append("upper_body:=true")
+        cmds.append("battery:=true")
 
-        cmds_aux.append("legs:=" + legs)
-        cmds_aux.append("big_wheel:=" + big_wheel)
-        cmds_aux.append("upper_body:=" + upper_body)
-        cmds_aux.append("velodyne:=" + velodyne)
-        cmds_aux.append("realsense:=" + realsense)
-        cmds_aux.append("floating_joint:=" + floating_joint)
-        cmds_aux.append("use_abs_mesh_paths:=true") # use absolute paths for meshes             \       
-        cmds_aux.append("root:=" + urdf_descr_root_path)
+        cmds.append("velodyne:=false")
+        cmds.append("realsense:=false")
+        cmds.append("floating_joint:=false")
+        cmds.append("use_abs_mesh_paths:=true") # use absolute paths for meshes    
+        
+        cmds.append("end_effector_left:=ball")
+        cmds.append("end_effector_right:=ball")
+        
+        cmds.append("root:=" + urdf_descr_root_path)
 
-        return cmds_aux
+        return cmds
 
 def get_xrdf_cmds_horizon_centauro(urdf_descr_root_path: str = None):
 
         cmds = []
         
-        xrdf_cmd_vals = [True, True, True, False, False, True] # horizon needs 
-        # the floating base
-        legs = "true" if xrdf_cmd_vals[0] else "false"
-        big_wheel = "true" if xrdf_cmd_vals[1] else "false"
-        upper_body ="true" if xrdf_cmd_vals[2] else "false"
-        velodyne = "true" if xrdf_cmd_vals[3] else "false"
-        realsense = "true" if xrdf_cmd_vals[4] else "false"
-        floating_joint = "true" if xrdf_cmd_vals[5] else "false"
-                
-        cmds.append("legs:=" + legs)
-        cmds.append("big_wheel:=" + big_wheel)
-        cmds.append("upper_body:=" + upper_body)
-        cmds.append("velodyne:=" + velodyne)
-        cmds.append("realsense:=" + realsense)
-        cmds.append("floating_joint:=" + floating_joint)
+        cmds.append("legs:=true")
+        cmds.append("big_wheel:=true")
+        cmds.append("upper_body:=true")
+        cmds.append("battery:=true")
+
+        cmds.append("velodyne:=false")
+        cmds.append("realsense:=false")
+        cmds.append("floating_joint:=true")
         cmds.append("use_abs_mesh_paths:=true") # use absolute paths for meshes             \       
+        
+        cmds.append("end_effector_left:=ball")
+        cmds.append("end_effector_right:=ball")
         
         if urdf_descr_root_path is not None:
                 cmds.append("root:=" + urdf_descr_root_path)
@@ -90,49 +84,30 @@ def get_xrdf_cmds_horizon_centauro(urdf_descr_root_path: str = None):
 
 def get_xrdf_cmds_kyon(urdf_descr_root_path: str = None):
 
-        cmds_aux = []
+        cmds = []
+
+        cmds.append("wheels:=false")
+        cmds.append("upper_body:=false")
+        cmds.append("dagana:=false")
+        cmds.append("sensors:=false")
+        cmds.append("floating_joint:=false")
+        cmds.append("payload:=false")
+        cmds.append("use_abs_mesh_paths:=true") # use absolute paths for meshes
         
-        xrdf_cmd_vals = [False, False, False, False, False, False]
+        cmds.append("root:=" + urdf_descr_root_path)
 
-        wheels = "true" if xrdf_cmd_vals[0] else "false"
-        upper_body = "true" if xrdf_cmd_vals[1] else "false"
-        gripper = "true" if xrdf_cmd_vals[2] else "false"
-        sensors = "true" if xrdf_cmd_vals[3] else "false"
-        floating_joint = "true" if xrdf_cmd_vals[4] else "false"
-        payload = "true" if xrdf_cmd_vals[5] else "false"
-
-        cmds_aux.append("wheels:=" + wheels)
-        cmds_aux.append("upper_body:=" + upper_body)
-        cmds_aux.append("dagana:=" + gripper)
-        cmds_aux.append("sensors:=" + sensors)
-        cmds_aux.append("floating_joint:=" + floating_joint)
-        cmds_aux.append("payload:=" + payload)
-        cmds_aux.append("use_abs_mesh_paths:=true") # use absolute paths for meshes
-        
-        cmds_aux.append("root:=" + urdf_descr_root_path)
-
-        return cmds_aux
+        return cmds
 
 def get_xrdf_cmds_horizon_kyon(urdf_descr_root_path: str = None):
 
         cmds = []
         
-        xrdf_cmd_vals = [False, False, False, False, True, False] # horizon needs 
-        # the floating base
-
-        wheels = "true" if xrdf_cmd_vals[0] else "false"
-        upper_body = "true" if xrdf_cmd_vals[1] else "false"
-        gripper = "true" if xrdf_cmd_vals[2] else "false"
-        sensors = "true" if xrdf_cmd_vals[3] else "false"
-        floating_joint = "true" if xrdf_cmd_vals[4] else "false"
-        payload = "true" if xrdf_cmd_vals[5] else "false"
-                
-        cmds.append("wheels:=" + wheels)
-        cmds.append("upper_body:=" + upper_body)
-        cmds.append("dagana:=" + gripper)
-        cmds.append("sensors:=" + sensors)
-        cmds.append("floating_joint:=" + floating_joint)
-        cmds.append("payload:=" + payload)
+        cmds.append("wheels:=false")
+        cmds.append("upper_body:=false")
+        cmds.append("dagana:=false")
+        cmds.append("sensors:=false")
+        cmds.append("floating_joint:=true")
+        cmds.append("payload:=false")
         
         if urdf_descr_root_path is not None:
                 cmds.append("root:=" + urdf_descr_root_path)
@@ -141,11 +116,11 @@ def get_xrdf_cmds_horizon_kyon(urdf_descr_root_path: str = None):
 
 def get_xrdf_cmds_b2w(urdf_descr_root_path: str = None):
 
-        cmds_aux = []
-        cmds_aux.append("use_abs_mesh_paths:=true") # use absolute paths for meshes
-        cmds_aux.append("root:=" + urdf_descr_root_path)
+        cmds = []
+        cmds.append("use_abs_mesh_paths:=true") # use absolute paths for meshes
+        cmds.append("root:=" + urdf_descr_root_path)
 
-        return cmds_aux
+        return cmds
 
 def get_xrdf_cmds_horizon_b2w(urdf_descr_root_path: str = None):
 
